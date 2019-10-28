4cb6cfa7c33e0c4f39d0085d8615b0fd6b0ecc2b
Stofey <54649442+Stofey@users.noreply.github.com>
2019-10-27 21:57:31 -0400

    Add project files.

---
+++ Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World ";
+            string name = " Amanda Stofey";
+            Console.WriteLine(greeting + "from" + name);
+        }
+    }
+}
